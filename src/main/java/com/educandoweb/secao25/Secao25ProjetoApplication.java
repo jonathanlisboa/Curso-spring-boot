8e5657080de99ff9d36845a400673a239e7b58d0
jonathan <jonathanslisboa@gmail.com>
2022-02-27 15:30:27 -0300

    Project created

---
+++ src/main/java/com/educandoweb/secao25/Secao25ProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.secao25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secao25ProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Secao25ProjetoApplication.class, args);
+	}
+
+}
